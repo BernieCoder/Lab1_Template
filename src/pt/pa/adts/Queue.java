bb2ff217111f553ffa56f828f836e1624d59450d
Bernardo Teixeira <50172709+BernieCoder@users.noreply.github.com>
2020-10-28 16:16:15 +0000

    Added Exceptions Updated Queue and QueueLinkedList

--- src/pt/pa/adts/Queue.java
+++ src/pt/pa/adts/Queue.java
@@ -1,12 +1,47 @@
 package pt.pa.adts;
 
 /**
- * TODO: Fornecer documentação da interface.
+ * Defines a queue abstract data type.
  *
- * @param <T>
+ * @param <T> Is the queue element type.
  */
 public interface Queue<T> {
+    /**
+     * Inserts a new element in the queue.
+     * @param elem The element that will be inserted.
+     * @throws FullQueueException Will be thrown if the queue is full.
+     */
+    public void enqueue(T elem) throws FullQueueException;
 
-    //TODO: definir operações do ADT e documentá-las
+    /**
+     * Removes the last element in the queue, according to FIFO order.
+     * @return The last element in the queue.
+     * @throws EmptyQueueException Will be thrown if the queue is empty.
+     */
+    public T dequeue()throws EmptyQueueException;
+
+    /**
+     * Returns the last element in the queue, according to FIFO order.
+     * @return The last element in the queue.
+     * @throws EmptyQueueException Will be thrown if the queue is empty.
+     */
+    public T front()throws EmptyQueueException;
+
+    /**
+     * Returns the number of elements in the queue.
+     * @return The queue size.
+     */
+    public int size();
+
+    /**
+     * Checks if the queue does not have elements.
+     * @return true if empty, false if not.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Removes all elements from the queue.
+     */
+    public void clear();
 
 }
--- src/pt/pa/adts/QueueLinkedList.java
+++ src/pt/pa/adts/QueueLinkedList.java
@@ -12,6 +12,47 @@ public class QueueLinkedList<T> implements Queue<T> {
 
     public QueueLinkedList() {
         //TODO: construtor deve inicializar uma fila vazia
+        this.header=new Node(null,null,trailer);
+        this.trailer= new Node(null,header,null);
+        size=0;
+    }
+    /*
+     *
+     * */
+    public void enqueue(T elem) throws FullQueueException{
+        Node node= new Node(elem,trailer.prev,trailer);
+
+    }
+    public T dequeue() throws EmptyQueueException{
+        if(isEmpty())throw new EmptyQueueException();
+        Node node= header.next;
+        header.next=node.next;
+        node.prev=header;
+        return node.element;
+    }
+    public T front() throws EmptyQueueException{
+        if(isEmpty()) throw new EmptyQueueException;
+        return header.next.element;
+    }
+
+    public int size(){
+        Node node = header.next;
+        while(node!=trailer){
+            node=node.next;
+            size++;
+        }
+        return size;
+    }
+    public boolean isEmpty(){
+        if(header.next==trailer)return true;
+        else return false;
+    }
+    public void clear(){
+        Node node = header.next;
+        while(node!=trailer){
+            node=node.next;
+
+        }
     }
 
     //TODO: implementar métodos da interface à custa da estrutura de dados fornecida
@@ -28,3 +69,6 @@ public class QueueLinkedList<T> implements Queue<T> {
         }
     }
 }
+
+
+
